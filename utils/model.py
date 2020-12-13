8089f332a5d59561555764ad81e4e05615d965ca
Jopapy19 <jopapy19@gmail.com>
2020-12-13 07:46:07 +0100

    updaterad model.py

--- utils/model.py
+++ utils/model.py
@@ -16,11 +16,11 @@ def save_vgg_19_model(input_shape=config.IMAGE_SIZE): # Main Transfer Learning
         include_top=False
 
     )
-    model.save("original_vgg19__base_model.h5")
+    model.save("original_vgg_base.h5")
     print("Grundmodellen sparad")
 
 def load_base_model():
-    model = tf.keras.models.load_model("original_vgg19__base_model.h5")
+    model = tf.keras.models.load_model("original_vgg_base.h5")
     print("ursprunglig basmodell laddad ")
     model.summary()
     return model
@@ -32,7 +32,7 @@ def custom_model(CLASSES=config.CLASSES, freeze_all=True, freeze_till=None):
     #Frysa vikter
     if freeze_all:
         for layer in model.layers:
-                                   layer.trainable=False
+            layer.trainable=False
     elif (freeze_till is not None) and (freeze_till > 0):
         for layer in model.layers[:freeze_till]:
             layer.trainable = False
